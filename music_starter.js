064a1e368eb1dd24a000111fe038438fcd08f972
ezmhme <emzhemingway@gmail.com>
2023-09-28 11:35:39 +1300

    Progress

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,62 @@
-
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Helvetica'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
+   background(0);
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
+   var ballSize = map(other, 0, 100, 0, 200);
+   var ballSize2 = map(vocal, 0, 100, 0, 200);
+   var ballSize3 = map(drum, 0, 100, 0, 200);
+   var ballSize4 = map(bass, 0, 100, 0, 200);
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+   noStroke();
+   fill(255, 60, 0);
+   ellipse(150,150,ballSize,ballSize);
+   fill(255, 106, 13);
+   ellipse(150,150,ballSize/1.25, ballSize/1.25);
+   fill(126, 255, 71);
+   ellipse(150,150,ballSize/2, ballSize/2);
+   fill(13, 17, 255);
+   ellipse(150,150,ballSize/3.5, ballSize/3.5);
+
+   noStroke();
+   fill(255, 60, 0);
+   ellipse(400,280,ballSize2,ballSize2);
+   fill(255, 106, 13);
+   ellipse(400,280,ballSize2/1.25, ballSize2/1.25);
+   fill(126, 255, 71);
+   ellipse(400,280,ballSize2/2, ballSize2/2);
+   fill(13, 17, 255);
+   ellipse(400,280,ballSize2/3.5, ballSize2/3.5);
+
+   noStroke();
+   fill(255, 60, 0);
+   ellipse(170,500,ballSize3,ballSize3);
+   fill(255, 106, 13);
+   ellipse(170,500,ballSize3/1.25, ballSize3/1.25);
+   fill(126, 255, 71);
+   ellipse(170,500,ballSize3/2, ballSize3/2);
+   fill(13, 17, 255);
+   ellipse(170,500,ballSize3/3.5, ballSize3/3.5);
+
+   noStroke();
+   fill(255, 60, 0);
+   ellipse(440,580,ballSize4,ballSize4);
+   fill(255, 106, 13);
+   ellipse(440,580,ballSize4/1.25, ballSize4/1.25);
+   fill(126, 255, 71);
+   ellipse(440,580,ballSize4/2, ballSize4/2);
+   fill(13, 17, 255);
+   ellipse(440,580,ballSize4/3.5, ballSize4/3.5);
+
+   // noStroke();
+   // fill(255,60,0);
+   // ellipse(265,500,ballSize,ballSize);
+
+   // fill(255,106,13);
+   // ellipse(265,500,ballSize2/1,ballSize2/1);
+
+   // fill(126,255,71);
+   // ellipse(265,500,ballSize3/2, ballSize3/2);
+
+   // fill(13,17,255);
+   // ellipse(265,500,ballSize4/3, ballSize4/3);
 }
